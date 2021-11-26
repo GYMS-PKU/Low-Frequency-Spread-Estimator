51af7767d037b1d16f21c903b76dd01168588bc3
GYMS-PKU <18316887409@163.com>
2021-11-26 17:47:46 +0800

    new Roll

--- estimator/statistical_estiamtor/Roll1999.py
+++ estimator/statistical_estiamtor/Roll1999.py
@@ -6,4 +6,24 @@
 Roll, R. (1984) A simple implicit measure of the effective bid-ask spread in an efficient market,
 Journal of Finance 39, 1127–1139.
 中的求协方差的方法来估计spread
+
+开发日志
+2021-11-26
+-- 实现pure python版本，后续应当实现cython版本，如果有加速效果
 """
+
+
+import numpy as np
+
+
+def roll(p, threshold=0.01):
+    """
+    :param p: 原始价格序列，为二维array，days * num_stock形状
+    :param threshold: 不正常价差估计的修正
+    :return: 返回股票数量的估计值
+    """
+    delta_p = p[1:] - p[:-1]
+    cov = np.sum(delta_p[1:]*delta_p[:-1], axis=0)
+    cov -= np.mean(delta_p[1:], axis=0) * np.mean(delta_p[:-1], axis=0)
+    cov[cov > -threshold**2] = -threshold ** 2
+    return -2 * np.sqrt(-cov)
